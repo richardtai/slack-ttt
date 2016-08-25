edfa86e651623fd855e96ff455d4c8ff2b2ca1f1
Richard Tai <ritai@microsoft.com>
2016-08-25 11:13:41 -0700

    Changed mongo path

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ var app = express();
 mongoose.Promise = global.Promise;
 
 // connect to MongoDB
-mongoose.connect('mongodb://localhost/slack-tictactoe-api')
-	.then(() => console.log('Mongodb connection successful.'))
-	.catch((err) => console.log(err));
+mongoose.connect(process.env.MONGOLAB_URI)
+ .then(() => console.log('Mongodb connection successful.'))
+ .catch((err) => console.log(err));
+// mongoose.connect('mongodb://localhost/slack-tictactoe-api')
+// 	.then(() => console.log('Mongodb connection successful.'))
+// 	.catch((err) => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
